f50a600ef17e084b56d982d776c2c5b203bd4f1c
ccyd <hawangyudong@outlook.com>
2023-06-03 12:14:53 +0800

    fix: 删除key_search的import

--- backend/search/do_search/urls.py
+++ backend/search/do_search/urls.py
@@ -2,12 +2,11 @@
 路由配置
 """
 from django.urls import path, include
-from .views import text_search, key_search, similar_search, test_similar
+from .views import text_search, similar_search, test_similar
 
 
 urlpatterns = [
     path('text_search/', text_search),
-    path('key_search/', key_search),
     path('similar_search/', similar_search),
     path('test_similar/', test_similar),
 ]
